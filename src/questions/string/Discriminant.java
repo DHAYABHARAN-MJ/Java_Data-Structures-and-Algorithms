0b6ebd4a317498215aa4112ca1f99363e7073c44
dhaya <dhayabharanmj@gmail.com>
2025-02-26 09:27:23 +0530

    new problem added

---
+++ src/questions/string/Discriminant.java
@@ -0,0 +1,27 @@
+package questions.string;
+
+import java.util.Scanner;
+
+public class Discriminant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i1=sc.nextInt();
+        int i2=sc.nextInt();
+        int i3=sc.nextInt();
+        String res = solutions.ans(i1,i2,i3);
+        System.out.println(res);
+    }
+}
+class solutions{
+    public static String ans(int i1,int i2,int i3){
+        StringBuffer sb = new StringBuffer();
+        int ans = i2*i2-4*i1*i3;
+        if(ans>=0) {
+            sb.append(" is real and positive roots");
+        }
+        else{
+            sb.append(" is imaginary and negative roots");
+        }
+        return "Discriminant:  "+ ans + sb.toString();
+    }
+}
---
+++ src/questions/string/ReverseString.java
@@ -0,0 +1,24 @@
+package questions.string;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String res = reverse(str);
+        System.out.println(res);
+    }
+    public static String reverse(String str) {
+        int left = 0, right = str.length() - 1;
+        StringBuffer sb = new StringBuffer(str);
+        while (left < right) {
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left++;
+            right--;
+        }
+        return sb.toString();
+    }
+}
