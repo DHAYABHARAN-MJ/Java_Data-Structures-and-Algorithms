601bbb9a364349c7ca478bdbfeb37a18c8f9d3dc
dhaya <dhayabharanmj@gmail.com>
2025-03-12 14:30:53 +0530

    Added: Merge Sort

--- src/Algorithms/Sorting/MergeSort.java
+++ src/Algorithms/Sorting/MergeSort.java
@@ -1,4 +1,73 @@
-/*
-Sample text:merge sort
-It is a recursive function
- */
+package Algorithms.Sorting;
+
+import java.util.*;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        mergesort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void mergesort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        mergesort(arr, 0, arr.length - 1);
+    }
+
+    private static void mergesort(int[] arr, int left, int right) {
+        if (left < right) {
+            int middle = left + (right - left) / 2;
+            mergesort(arr, left, middle);
+            mergesort(arr, middle + 1, right);
+            merge(arr, left, middle, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArray[j] = arr[middle + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+}
